0f6e71bac6b010ec2bac55b6fc50607a3dfddb2c
Ivan Kahl <hello@ivankahl.com>
2020-04-11 00:44:43 +0200

    Added netlify functions skeleton

---
+++ functions/getStories.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase/app';
+
+exports.handler = async (event, context) => {
+  //const app = firebase.initializeApp({ databaseURL:  })
+
+  return {
+    statusCode: 200,
+    body: `${process.env.HN_DATABASE_URL} ${process.env.HN_VERSION}`
+  }
+}
