2f8d3a99653ed8779abfa11aee10ae44bd414852
Ivan Kahl <ivan@mercury-fx.com>
2020-04-11 20:57:35 +0200

    Finished creating function to get feeds

--- functions/getFeeds.js
+++ functions/getFeeds.js
@@ -1,10 +1,39 @@
-import firebase from 'firebase/app';
+import axios from 'axios';
+
+const FEEDS = ['top', 'new', 'best', 'ask', 'show', 'job'];
 
 exports.handler = async (event, context) => {
-  //const app = firebase.initializeApp({ databaseURL:  })
-
-  return {
-    statusCode: 200,
-    body: `${process.env.HN_DATABASE_URL} ${process.env.HN_VERSION} ${event.name}`
+  // Get the feed name and validate it
+  const feedName = event.path.split('/').slice(-1)[0];
+  if (!FEEDS.includes(feedName)) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ error: 'The specified feed could not be found. Please specify a valid feed.' })
+    };
   }
+
+  // Get paging parameters if possible
+  const n = isNaN(parseInt(event.queryStringParameters['n'])) ? 10 : parseInt(event.queryStringParameters['n']);
+  const p = isNaN(parseInt(event.queryStringParameters['p'])) ? 1 : parseInt(event.queryStringParameters['p']);
+
+  try {
+    // Get the story IDs
+    const storyIds = (await axios.get(`${process.env.HN_DATABASE_URL}/${feedName}stories.json`)).data.slice((p - 1) * n, p * n);
+
+    // Retrieve each story asynchronously
+    const stories = await Promise.all(storyIds.map(async (storyId) => {
+      return (await axios.get(`${process.env.HN_DATABASE_URL}/item/${storyId}.json`)).data
+    }));
+
+    // Return the stories on the page
+    return {
+      statusCode: 200,
+      body: JSON.stringify(stories)
+    }
+  } catch(e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: e.toString() })
+    };
+  } 
 }
